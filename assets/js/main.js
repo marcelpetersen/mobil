35296e453da2ed8077bb561034945452351907c4
bkammerling <bkammerling@googlemail.com>
2019-02-05 14:37:50 +0100

    contact form extra fields in

--- assets/js/main.js
+++ assets/js/main.js
@@ -448,7 +448,6 @@ var benefits = {
 }
 if(pagetitle == "Careers") benefits.init();
 
-
 var scroller = {
   header: $("nav.absolute-header"),
   init: function() {
@@ -486,3 +485,50 @@ var scroller = {
 }
 
 scroller.init();
+
+
+// Select all links with hashes
+$('a[href*="#"]')
+// Remove links that don't actually link to anything
+.not('[href="#"]')
+.not('[href="#0"]')
+.click(function(event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+    &&
+    location.hostname == this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top - 80
+      }, 600, function() {
+        // Callback after animation
+        // Must change focus!
+        var $target = $(target);
+        $target.focus();
+        if ($target.is(":focus")) { // Checking if the target was focused
+          return false;
+        } else {
+          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+          $target.focus(); // Set focus again
+        };
+      });
+    }
+  }
+});
+
+$("form .dropdown a").click(function(e) {
+  var selection = $(this).data('item');
+  console.log(selection);
+  $("form .dropdown button").text(selection);
+  $("form .extra-form ."+selection).show();
+  $("form .extra-form > div:not(."+selection+")").hide();
+  $("form .hidden").removeClass("hidden");
+});
