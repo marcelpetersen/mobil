e88b0c374c010d168b83172af63f5c860390413e
bkammerling <bkammerling@googlemail.com>
2019-10-01 16:30:30 +0200

    speeding up title animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,7 @@ window.addEventListener('load', function() {
 });
 
 //set animation timing
-var animationDelay = 3500,
+var animationDelay = 2500,
 	//loading bar effect
 	barAnimationDelay = 3800,
 	barWaiting = barAnimationDelay - 3000, //3000 is the duration of the transition on the loading bar - set in the scss/css file
