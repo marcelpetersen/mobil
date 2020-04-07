77a599f80efca6e9c046f8bf88198e0042f09a02
bkammerling <bkammerling@googlemail.com>
2020-04-07 14:56:23 +0200

    added many operators

--- assets/js/components/weAllMove.js
+++ assets/js/components/weAllMove.js
@@ -1,3 +1,9 @@
+var filterObj = {
+  need: '',
+  city: '',
+  country: ''
+}
+
 $(document).ready(function() {
 
   $("#citySelect").select2({
@@ -15,5 +21,23 @@ $(document).ready(function() {
     minimumResultsForSearch: 15,
     placeholder: "Need"
   });
+
+
+  $('.we-select2').on("change", function (e) {
+    console.log("change");
+    filterObj[e.target.name] = e.target.value;
+    filterList();
+  });
+
+  function filterList() {
+    $(`.weallmove-card:not(:contains(${filterObj.need}):contains(${filterObj.city}):contains(${filterObj.country}))`).addClass('card-hidden');
+    $(`.weallmove-card:contains(${filterObj.need}):contains(${filterObj.city}):contains(${filterObj.country})`).removeClass('card-hidden');
+    $('.current-showing').text($('.weallmove-card:not(.card-hidden)').length);
+    if($('.weallmove-card:not(.card-hidden)').length == 0) {
+      $('.noshow-feedback').removeClass('hide');
+    } else {
+      $('.noshow-feedback').addClass('hide');
+    }
+  }
 
 });
