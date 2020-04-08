a6498573486c628ded1f27e28d35ef6c5fa982a9
bkammerling <bkammerling@googlemail.com>
2020-04-08 09:31:00 +0200

    nother update weallmove

--- assets/js/components/weAllMove.js
+++ assets/js/components/weAllMove.js
@@ -27,9 +27,28 @@ $(document).ready(function() {
     console.log("change");
     filterObj[e.target.name] = e.target.value;
     filterList();
+
+    console.log(countryObject[e.target.value]);
+    if(e.target.name == 'country' && e.target.value) {
+      $("#citySelect").select2('destroy').empty().select2({
+        data: countryObject[e.target.value],
+        allowClear: true,
+        minimumResultsForSearch: 15,
+        placeholder: "City"
+      });
+    } else if(e.target.name == 'country' && !e.target.value) {
+      $("#citySelect").select2('destroy').empty().select2({
+        data: cityArray,
+        allowClear: true,
+        minimumResultsForSearch: 15,
+        placeholder: "City"
+      });
+    }
+
   });
 
   function filterList() {
+    console.log(filterObj);
     $(`.weallmove-card:not(:contains(${filterObj.need}):contains(${filterObj.city}):contains(${filterObj.country}))`).addClass('card-hidden');
     $(`.weallmove-card:contains(${filterObj.need}):contains(${filterObj.city}):contains(${filterObj.country})`).removeClass('card-hidden');
     $('.current-showing').text($('.weallmove-card:not(.card-hidden)').length);
